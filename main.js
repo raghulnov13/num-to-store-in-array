4d1c46097f5f64ed86f6eb1db2bf1f8879290ae0
Raghul <raghulnov13@gmail.com>
2022-09-19 15:36:48 +0530

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+let email = "ravanth39840@gmail.com";
+
+let a = email.replace(/[^0-9]/g, "");
+console.log(a);
+let b =a.split("");
+console.log(b);
+
+for (let i = 0; i < b.length; i++) {
+    if (b[i] % 2 === 0) {
+        console.log(b[i]);
+    }
+}
